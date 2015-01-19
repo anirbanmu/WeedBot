87258f525c34d3f4a1c92c78fd9ba039073ae890
Foxlet <inquiries@comprepair.tk>
2015-01-19 12:38:22 -0500

    We assume Unicode now.

--- plugins/weedbotsayings.py
+++ plugins/weedbotsayings.py
@@ -1,78 +1,80 @@
+# WeedBot Refresh's weedbotsayings.py - Py3 version
+
 import random
-from util import hook
+from cloudbot import hook
 import re
-cs = unicode("( .__.) . o O ( cum )", encoding="utf-8")
-ms = unicode("( ï¾Ÿ ãƒ®ï¾Ÿ)ã€€ï¼­ï¼©ï¼´ï¼¯ï¼® ï¼§ï¼¡ ï¼³ï¼µï¼«ï¼©ï¼ï¼ï¼ï¼", encoding="utf-8")
-cp = unicode("( ãƒ»Ï‰ãƒ») . o O ( crap ass )", encoding="utf-8")
-ft = unicode("ãƒ¾(Â´â–¡ï½€* )ãƒŽ ftw ãƒ¾(@ã‚œâˆ‡ã‚œ@)ãƒŽ ftw o(â‰§âˆ‡â‰¦o) ftw (Â´âˆ‡ï¾‰ï½€*)ãƒŽ", encoding="utf-8")
-ap = unicode("( ï½¥à¸´Ð·ï½¥à¸´) apple basically makes the best computers.. heh", encoding="utf=8")
-da = unicode("Î¾ ãƒ»_>ãƒ»ï¼‰don't touch dad's diskettes", encoding="utf-8")
-ta = unicode("(â•¯Â°â–¡Â°ï¼‰â•¯ï¸µ â”»â”â”» i'm so fuckin mad right now", encoding="utf-8")
-gi = unicode("ï½·ï¾€ï¾œã‚¡*ï½¥ã‚œï¾Ÿï½¥*:.ï½¡..ï½¡.:*ï½¥ã‚œ(nâ€˜âˆ€â€˜)Î·ï¾Ÿï½¥*:.ï½¡. .ï½¡.:*ï½¥ã‚œï¾Ÿï½¥* !!!!! oh my god i'm a girl!!!", encoding="utf-8")
+cs = "( .__.) . o O ( cum )"
+ms = "( ï¾Ÿ ãƒ®ï¾Ÿ)ã€€ï¼­ï¼©ï¼´ï¼¯ï¼® ï¼§ï¼¡ ï¼³ï¼µï¼«ï¼©ï¼ï¼ï¼ï¼"
+cp = "( ãƒ»Ï‰ãƒ») . o O ( crap ass )"
+ft = "ãƒ¾(Â´â–¡ï½€* )ãƒŽ ftw ãƒ¾(@ã‚œâˆ‡ã‚œ@)ãƒŽ ftw o(â‰§âˆ‡â‰¦o) ftw (Â´âˆ‡ï¾‰ï½€*)ãƒŽ"
+ap = "( ï½¥à¸´Ð·ï½¥à¸´) apple basically makes the best computers.. heh"
+da = "Î¾ ãƒ»_>ãƒ»ï¼‰don't touch dad's diskettes"
+ta = "(â•¯Â°â–¡Â°ï¼‰â•¯ï¸µ â”»â”â”» i'm so fuckin mad right now"
+gi = "ï½·ï¾€ï¾œã‚¡*ï½¥ã‚œï¾Ÿï½¥*:.ï½¡..ï½¡.:*ï½¥ã‚œ(nâ€˜âˆ€â€˜)Î·ï¾Ÿï½¥*:.ï½¡. .ï½¡.:*ï½¥ã‚œï¾Ÿï½¥* !!!!! oh my god i'm a girl!!!"
 
 @hook.nonick
 @hook.regex(*("cum", re.I))
 @hook.randreply(0.0)
-def cum(inp):
+def cum(text):
     return cs
 
 @hook.nonick
 @hook.regex(*("mitten", re.I))
 @hook.randreply(0.0)
-def mitten(inp):
+def mitten(text):
     return ms
 
 @hook.nonick
 @hook.regex(*("crap", re.I))
 @hook.randreply(0.0)
-def crap(inp):
+def crap(text):
     return cp
 
 @hook.nonick
 @hook.regex(*("ass", re.I))
 @hook.randreply(0.0)
-def ass(inp):
+def ass(text):
     return cp
 
 @hook.nonick
 @hook.regex(*("\\bftw\\b", re.I))
 @hook.randreply(0.0)
-def ftw(inp):
+def ftw(text):
     return ft
 
 @hook.nonick
 @hook.regex(*("\\bown(s|ed)?\\b", re.I))
 @hook.randreply(0.0)
-def owns(inp):
-    ons = unicode("owns "*random.randint(1, 3) + "OWNS "*random.randint(1, 3) + "o-(' 'Q)")
+def owns(text):
+    ons = "owns " * random.randint(1, 3) + "OWNS " * random.randint(1, 3) + "o-(' 'Q)"
     return ons
 
 @hook.nonick
 @hook.regex(*("\\bapple\\b", re.I))
 @hook.randreply(0.0)
-def apple(inp):
+def apple(text):
     return ap
 
 @hook.nonick
 @hook.regex(*("weedbot", re.I))
 @hook.randreply(0.0)
-def weedbotname(inp):
+def weedbotname(text):
     return random.choice([":D", "hi!", "weed", "gotta love me!", "guess who has a boner", "weeeeeeed", "weeeeeeeeeeeeeeeeed"])
 
 @hook.nonick
 @hook.regex(*("\\bdad\\b", re.I))
 @hook.randreply(0.0)
-def dad(inp):
+def dad(text):
     return da
 
 @hook.nonick
 @hook.regex(*("\\b(mad|angry)\\b", re.I))
 @hook.randreply(0.0)
-def mad(inp):
+def mad(text):
     return ta
 
 @hook.nonick
 @hook.regex(*("\\bgirl\\b", re.I))
 @hook.randreply(0.0)
-def girl(inp):
+def girl(text):
     return gi
