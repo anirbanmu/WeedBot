1197ef40c71eb8406217c62f2dee216047c59de8
Foxlet <inquiries@comprepair.tk>
2015-01-19 19:04:56 -0500

    Update Regex

--- plugins/weedbotsayings.py
+++ plugins/weedbotsayings.py
@@ -13,57 +13,57 @@ ta = "(â•¯Â°â–¡Â°ï¼‰â•¯ï¸µ â”»â”â”» i'm so f
 gi = "ï½·ï¾€ï¾œã‚¡*ï½¥ã‚œï¾Ÿï½¥*:.ï½¡..ï½¡.:*ï½¥ã‚œ(nâ€˜âˆ€â€˜)Î·ï¾Ÿï½¥*:.ï½¡. .ï½¡.:*ï½¥ã‚œï¾Ÿï½¥* !!!!! oh my god i'm a girl!!!"
 
 
-@hook.regex(*("cum", re.I))
+@hook.regex(re.compile(r'cum', re.I))
 def cum(bot):
     bot.reply(cs)
 
 
-@hook.regex(*("mitten", re.I))
+@hook.regex(re.compile(r'mitten', re.I))
 def mitten(bot):
     bot.reply(ms)
 
 
-@hook.regex(*("crap", re.I))
+@hook.regex(re.compile(r'crap', re.I))
 def crap(bot):
     bot.reply(cp)
 
 
-@hook.regex(*("ass", re.I))
+@hook.regex(re.compile(r'ass', re.I))
 def ass(bot):
     bot.reply(cp)
 
 
-@hook.regex(*("\\bftw\\b", re.I))
+@hook.regex(re.compile(r'\\bftw\\b', re.I))
 def ftw(bot):
     bot.reply(ft)
 
 
-@hook.regex(*("\\bown(s|ed)?\\b", re.I))
+@hook.regex(re.compile(r'\\bown(s|ed)?\\b', re.I))
 def owns(bot):
     ons = "owns " * random.randint(1, 3) + "OWNS " * random.randint(1, 3) + "o-(' 'Q)"
     bot.reply(ons)
 
 
-@hook.regex(*("\\bapple\\b", re.I))
+@hook.regex(re.compile(r'\\bapple\\b', re.I))
 def apple(bot):
     bot.reply(ap)
 
 
-@hook.regex(*("weedbot", re.I))
+@hook.regex(re.compile(r'weedbot', re.I))
 def weedbotname(bot):
     bot.reply(random.choice([":D", "hi!", "weed", "gotta love me!", "guess who has a boner", "weeeeeeed", "weeeeeeeeeeeeeeeeed"]))
 
 
-@hook.regex(*("\\bdad\\b", re.I))
+@hook.regex(re.compile(r'\\bdad\\b', re.I))
 def dad(bot):
     bot.reply(da)
 
 
-@hook.regex(*("\\b(mad|angry)\\b", re.I))
+@hook.regex(re.compile(r'\\b(mad|angry)\\b', re.I))
 def mad(bot):
     bot.reply(ta)
 
 
-@hook.regex(*("\\bgirl\\b", re.I))
+@hook.regex(re.compile(r'\\bgirl\\b', re.I))
 def girl(bot):
     bot.reply(gi)
